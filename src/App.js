173fbe305f99d6d21015d070532f09be67d2f3b0
drewberrysteph <drewberrysteph@gmail.com>
2023-06-07 17:03:24 +0800

    add counter func and test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  const handleIncrement = () => setCounter((counter) => counter + 1);
+
+  const handleDecrement = () => setCounter((counter) => counter - 1);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn CI
+      </a>
+      <h1>Counter: {counter}</h1>
+      <button onClick={handleIncrement}> Increment </button>
+      <button onClick={handleDecrement}> Decrement </button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
